c48250e5ee4ebe4334a22ad6547d79e16fee1a8b
moisesrj97 <moisesrj97@gmail.com>
2022-02-15 11:48:38 +0100

    Create car interface

---
+++ src/interfaces/carInterface.ts
@@ -0,0 +1,12 @@
+export interface CarI {
+  brand: string;
+  model: string;
+  period: string;
+  cc: string;
+  cylinders: string;
+  fuel: string;
+  kw: string;
+  cvf: string;
+  cv: string;
+  value: string;
+}
