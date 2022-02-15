4fd3eec6f44e1dc4a9db325f84fde1d457377d15
moisesrj97 <moisesrj97@gmail.com>
2022-02-15 13:34:52 +0100

    Add Form component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>Swipo</h1>
+      <h1>Swipoo entry task</h1>
+      <Form />
     </div>
   );
 }
