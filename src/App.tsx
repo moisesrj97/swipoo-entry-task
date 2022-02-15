2fde1280261fa43c56cf9c574ff9f6bafb563049
moisesrj97 <moisesrj97@gmail.com>
2022-02-15 17:16:19 +0100

    Add App component styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import Form from './components/Form/Form';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <h1>Swipoo entry task</h1>
-      <Form />
+    <div className="w-screen h-screen flex flex-col my-10 items-center">
+      <div className="w-10/12 h-max py-4 px-6 flex flex-col items-center rounded-md shadow shadow-gray-400">
+        <h2 className="text-1xl text-left w-full text-gray-500">
+          Hi, welcome to my
+        </h2>
+        <h1 className="uppercase text-2xl w-full mb-2 font-bold text-left underline underline-offset-2 decoration-blue-500">
+          Swipoo entry task
+        </h1>
+        <Form />
+      </div>
     </div>
   );
 }
