62726112a332a7281d55adb3ede12becf50ce2ad
moisesrj97 <moisesrj97@gmail.com>
2022-02-15 14:04:55 +0100

    Add car details render

--- src/components/CarDetail.tsx
+++ src/components/CarDetail.tsx
@@ -5,8 +5,33 @@ interface PropsI {
   selectedCar: CarI;
 }
 
-function CarDetail({ selectedCar }: PropsI): JSX.Element {
-  return <div>{JSON.stringify(selectedCar)}</div>;
+function CarDetail({
+  selectedCar: {
+    brand,
+    cc,
+    cv,
+    cvf,
+    cylinders,
+    fuel,
+    kw,
+    model,
+    period,
+    value,
+  },
+}: PropsI): JSX.Element {
+  return (
+    <div>
+      <h3>{`${model} (${brand})`}</h3>
+      <p>{`CC: ${cc}`}</p>
+      <p>{`CV: ${cv}`}</p>
+      <p>{`CVF: ${cvf}`}</p>
+      <p>{`Cylinders: ${cylinders}`}</p>
+      <p>{`Fuel: ${fuel}`}</p>
+      <p>{`KW: ${kw}`}</p>
+      <p>{`Period: ${period}`}</p>
+      <p>{`Value: ${value}`}</p>
+    </div>
+  );
 }
 
 export default CarDetail;
