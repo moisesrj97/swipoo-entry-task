330b61e3fde17761bfe638559c4080aba2905a6e
moisesrj97 <moisesrj97@gmail.com>
2022-02-16 10:33:52 +0100

    Add responsive behaviour to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Form from './components/Form/Form';
 function App(): JSX.Element {
   return (
     <div className="w-screen flex flex-col my-10 items-center">
-      <div className="w-11/12 h-max py-4 px-6 flex flex-col items-center rounded-md shadow shadow-gray-400">
+      <div className="w-11/12 md:w-8/12 xl:w-6/12 2xl:w-4/12 h-max py-4 px-6 md:px-6 xl:px-8 flex flex-col items-center rounded-md shadow shadow-gray-400">
         <h2 className="text-1xl text-left w-full text-gray-500">
           Hi, welcome to my
         </h2>
