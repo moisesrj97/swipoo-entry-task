653bf9171cfaa72850fc7dcfa30e9937a870fc7b
moisesrj97 <moisesrj97@gmail.com>
2022-02-16 12:01:13 +0100

    Add error handling test in apiCall service

--- src/services/apiCallService.spec.ts
+++ src/services/apiCallService.spec.ts
@@ -11,4 +11,16 @@ describe('Given apiCallServices', () => {
       expect(await getCars('Audi', '2020-01-01', 'G')).toEqual([]);
     });
   });
+  describe('When getCars is called without errors', () => {
+    beforeEach(() => {
+      global.fetch = jest
+        .fn()
+        .mockRejectedValue(new Error('Fetch error')) as jest.Mock;
+    });
+    test('It should return data and call fetch', () => {
+      getCars('Audi', '2020-01-01', 'G').catch((e) => {
+        expect(e.message).toEqual('Error: Fetch error');
+      });
+    });
+  });
 });
